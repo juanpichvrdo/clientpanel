bf96e947853b2c53c07b6af5cb118242729f8141
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-07 00:03:10 -0400

    Added layout, set up redux and firestore, connected firestore data to Clients component.

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+
+class Clients extends Component {
+  render() {
+    const { clients } = this.props;
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                {" "}
+                <i className="fas fa-users" /> Clients{" "}
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/clients/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
+  }
+}
+
+Clients.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array
+};
+
+export default compose(
+  firestoreConnect([{ collection: "clients" }]),
+  connect((state, props) => ({
+    clients: state.firestore.ordered.clients
+  }))
+)(Clients);
---
+++ src/store.js
@@ -0,0 +1,54 @@
+import { createStore, combineReducers, compose } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
+import { reduxFirestore, firestoreReducer } from "redux-firestore";
+
+// Reducers
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWsSAOXeZjK7bjfMZnZ6KsmDa6Fx5shXc",
+  authDomain: "clientpanel-75915.firebaseapp.com",
+  databaseURL: "https://clientpanel-75915.firebaseio.com",
+  projectId: "clientpanel-75915",
+  storageBucket: "clientpanel-75915.appspot.com",
+  messagingSenderId: "951791905150"
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Initialize firestore
+// const firestore = firebase.firestore();
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase)
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Create initial state
+const initialState = {};
+
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
