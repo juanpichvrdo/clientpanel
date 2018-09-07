39d419f9470ee0dd0b7cfd84460eb2e34fe35682
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-07 02:15:17 -0400

    Added a spinner gift to represent loading, added functionality to add clients to firestore

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -5,9 +5,28 @@ import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 
+import Spinner from "../layout/Spinner";
+
 class Clients extends Component {
+  state = {
+    totalOwed: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+
+    if (clients) {
+      const total = clients.reduce((total, client) => {
+        return total + parseFloat(client.balance.toString());
+      }, 0);
+
+      return { totalOwed: total };
+    }
+  }
+
   render() {
     const { clients } = this.props;
+    const { totalOwed } = this.state;
 
     if (clients) {
       return (
@@ -19,7 +38,14 @@ class Clients extends Component {
                 <i className="fas fa-users" /> Clients{" "}
               </h2>
             </div>
-            <div className="col-md-6" />
+            <div className="col-md-6">
+              <h5 className="text-right text-secondary">
+                Total Owed{" "}
+                <span className="text-primary">
+                  ${parseFloat(totalOwed).toFixed(2)}
+                </span>
+              </h5>
+            </div>
           </div>
 
           <table className="table table-striped">
@@ -54,7 +80,7 @@ class Clients extends Component {
         </div>
       );
     } else {
-      return <h1>Loading...</h1>;
+      return <Spinner />;
     }
   }
 }
