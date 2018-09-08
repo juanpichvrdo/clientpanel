bce7dd8c648f543fe2c2f995e08ffbe16376bea3
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-08 01:28:57 -0400

    Set up reducers and actions for settings component

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 
 import notifyReducer from "./reducers/notifyReducer";
+import settingsReducer from "./reducers/settingsReducer";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWsSAOXeZjK7bjfMZnZ6KsmDa6Fx5shXc",
@@ -39,7 +40,8 @@ const createStoreWithFirebase = compose(
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
-  notify: notifyReducer
+  notify: notifyReducer,
+  settings: settingsReducer
 });
 
 // Create initial state
